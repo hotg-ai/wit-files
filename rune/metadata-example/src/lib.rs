a904e38e0d239bfdf98a425bb733e43b994efd61
Michael-F-Bryan <michael@hotg.ai>
2022-02-27 21:19:31 +0800

    Added an example crate for playing around with metadata

---
+++ rune/metadata-example/src/lib.rs
@@ -0,0 +1,14 @@
+use runtime_v1::*;
+
+wit_bindgen_rust::import!("$CARGO_MANIFEST_DIR/../runtime-v1.wit");
+wit_bindgen_rust::export!("$CARGO_MANIFEST_DIR/../rune-v1.wit");
+
+pub struct RuneV1;
+
+impl rune_v1::RuneV1 for RuneV1 {
+    fn start() {
+        let meta = Metadata::new(env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+        meta.set_description(env!("CARGO_PKG_DESCRIPTION"));
+        meta.set_repository(env!("CARGO_PKG_REPOSITORY"));
+    }
+}
